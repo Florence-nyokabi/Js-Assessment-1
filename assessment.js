cd228bbe21b7d18aee491306f7cb9e6985a3c380
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-04-11 08:55:35 +0300

    assessment

---
+++ assessment.js
@@ -0,0 +1,80 @@
+// Reserse a strng
+// let food = eating;
+// function reverseString(food){
+//     return food.reverse();
+// }
+// console.log(reverseString(food));
+
+
+
+
+//binary search
+function searchTarget(num){
+    let target = 23;
+    let first = [0];
+    let last = [-1];
+
+    let middle = math.floor((last + first)/2);
+if 
+}
+
+let num = [2,8,0,23,5,45,76]
+
+// leap years
+let year = 2000;
+function isLeapYear(){
+    for(year = 2000 && 2023);{
+        if (year % 4 === 0){
+            console.log("${year} is a leap year");
+        }
+        else{
+            console.log("${year} is not a leap year");
+        }
+    }
+}
+console.log(isLeapYear(2004));
+
+// fizzbuzz
+// function fizzBuzz(){
+//     for( let i = 0, i <= 100, i++ ){
+//         if (i % 5 ===0 && i % 3 ===0){
+//             console.log("FizzBuzz");
+//         }
+//         else if (i % 3 === 0){
+//             console.log("Fizz");
+//         }
+//         else if (i % 5 === 0){
+//             console.log("Buzz");
+//         }
+//         else{
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzBuzz();
+
+// Question 5
+let numArray = [12,87,45,75,23,64,73];
+let mul = 1;
+function multiply(numArray){
+    for( i in numArray){
+       return (i * 4) 
+       i++;
+    }
+}
+console.log(multiply(numArray));
+
+// Question 6
+let nums = ["10", "24", "45", "56", "67"];
+function numbers(nums){
+    for( i in nums){
+        return i.tonumber();
+    }
+}
+
+
+
+
+
+
+
