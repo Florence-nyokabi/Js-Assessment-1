0b700f4db40ef72bbbeb6a4691e91eeb133c6649
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-04-11 09:08:09 +0300

    assessment

--- assessment.js
+++ assessment.js
@@ -1,29 +1,38 @@
-// Reserse a strng
-// let food = eating;
-// function reverseString(food){
-//     return food.reverse();
-// }
-// console.log(reverseString(food));
+// Question 1
+let food = eating;
+function reverseString(food){
+    return food.reverse();
+}
+console.log(reverseString(food));
 
 
 
 
-//binary search
+// Question 2
 function searchTarget(num){
     let target = 23;
     let first = [0];
     let last = [-1];
 
     let middle = math.floor((last + first)/2);
-if 
+    let emptyArray = [];
+
+    if(first < last){
+        console.log(num);
+    }
+    else if(last < first){
+        emptyArray =+
+        console.log(emptyArray);
+        
+    }
 }
 
 let num = [2,8,0,23,5,45,76]
 
-// leap years
+// Question 3
 let year = 2000;
 function isLeapYear(){
-    for(year = 2000 && 2023);{
+    for(year >= 2000 && year <= 2023){
         if (year % 4 === 0){
             console.log("${year} is a leap year");
         }
@@ -34,24 +43,24 @@ function isLeapYear(){
 }
 console.log(isLeapYear(2004));
 
-// fizzbuzz
-// function fizzBuzz(){
-//     for( let i = 0, i <= 100, i++ ){
-//         if (i % 5 ===0 && i % 3 ===0){
-//             console.log("FizzBuzz");
-//         }
-//         else if (i % 3 === 0){
-//             console.log("Fizz");
-//         }
-//         else if (i % 5 === 0){
-//             console.log("Buzz");
-//         }
-//         else{
-//             console.log(i);
-//         }
-//     }
-// }
-// fizzBuzz();
+// Question 4
+function fizzBuzz(){
+    for( let i = 0, i <= 100, i++){
+        if (i % 5 === 0 && i % 3 === 0){
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0){
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+fizzBuzz();
 
 // Question 5
 let numArray = [12,87,45,75,23,64,73];
@@ -68,7 +77,7 @@ console.log(multiply(numArray));
 let nums = ["10", "24", "45", "56", "67"];
 function numbers(nums){
     for( i in nums){
-        return i.tonumber();
+        return i.toNumber();
     }
 }
 
